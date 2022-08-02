635e2f1ce20b2fe7c57953b88fa51ddf1b2e69c7
Revanth <revanthhs@protonmail.com>
2022-08-02 11:53:29 +0530

    Made changes to the logic
    The logic is changed so that everything has its own function.
    Player and computer selection can be done.

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -6,35 +6,97 @@
 
 
 function getComputerChoice(){
-   return Math.floor( Math.random() * (4-1) + 1);   
+   let computerSelection = Math.floor( Math.random() * (4-1) + 1);   
+
+   
+    if(computerSelection === 1){
+        computerSelection = "rock";
+    }
+    else if(computerSelection === 2){
+        computerSelection = "paper";
+    }
+    else{
+        computerSelection = "scissors";
+    }
+    return computerSelection;
+    
 }
 
-let computerSelection = getComputerChoice();
-console.log(computerSelection);
 
-switch(computerSelection){
-    case 1:
-        if(computerSelection === 1){
-            computerSelection = "rock";
-        }
-        break;
-    case 2:
-        if(computerSelection === 2){
-            computerSelection = "paper";
-        }
-        break;
-    case 3:
-        if(computerSelection === 3){
-            computerSelection = "scissors";
-        }
-        break;
-}
-console.log(`Computer selection is ${computerSelection}`);
+
+
+
 
 //Player will select the option here which will be converted to lower case
-let playerSelection = prompt("Select between Rock Paper Scissors");
-playerSelection = playerSelection.toLowerCase();
-console.log(`Player selection is ${playerSelection}`);
+function getPlayerSelection()
+{
+    let playerSelection = prompt("Select between Rock Paper Scissors");
+    playerSelection = playerSelection.toLowerCase();
+    return playerSelection;
+}
+
+console.log(`Player selection is ${getPlayerSelection()}`);
+console.log(`Computer selection is ${getComputerChoice()}`);
 
 
+// Function of the game logic using if loop
+
+
+function getWinner(playerSelection,computerSelection)
+{
+    if(playerSelection === computerSelection)
+    {
+        console.log("Its a draw");
+    }
+    else if(playerSelection === "rock"){
+        if(computerSelection === "paper")
+        {
+            console.log("You Lose! Paper beats Rock");
+            computerScore++;
+
+        }
+        else
+        {
+            console.log("You Win! Rock beats Scissors");
+            playerScore++;
+        }
+    }
+    else if(playerSelection === "scissors"){
+        if(computerSelection === "paper")
+        {
+            console.log("You Win! Scissors beats Paper");
+            playerScore++;
+
+        }
+        else
+        {
+            console.log("You Lose! Rock beats Scissors");
+            computerScore++;
+        }
+    }
+    
+    console.log(`Player Score : ${playerScore}`);
+    console.log(`Computer Score : ${computerScore}`);
+}
+
+function game(){
+    for(let i=0;i>5;i++)
+    {
+        playRound();
+    }
+    
+}
+function playRound(){
+    playerSelection = getPlayerSelection();
+    computerSelection = getComputerChoice();
+    getWinner(playerSelection,computerSelection);
+}
+
+let computerScore = 0;
+let playerScore = 0;
+game();
+//console.log( playRound());
+//Function to run the game 5 times using for loop
+
+//
 
