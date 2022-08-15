66f5b78f4462a4807528aeb27f72f3ad47b3f4f7
Revanth <revanthhs@protonmail.com>
2022-08-16 02:17:41 +0530

    Added the logic for selection
    Added what the user selected based on the button clicked
    Added the computer selection function as well
    Logging both of them in console.

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -5,99 +5,98 @@
 
 
 
+const selected = document.querySelectorAll('button');
+
+//To get the clicks of the buttons we will be using this 
+selected.forEach(button => { button.addEventListener('click', getPlayerSelection)})
+
+
+
 //Player will select the option here which will be converted to lower case
-function getPlayerSelection()
-{
-    let playerSelection = prompt("Select between Rock Paper Scissors");
-    playerSelection = playerSelection.toLowerCase();
-    return playerSelection;
-}
+
 
 
 //To get the computer selection based on the math function
 function getComputerChoice(){
 
     let computerSelection = Math.floor( Math.random() * (4-1) + 1);   
+    //4 is max capped & 1 is the lowest limit
 
-    if(computerSelection === 1){
-         computerSelection = "rock";
-     }
-     else if(computerSelection === 2){
-         computerSelection = "paper";
-     }
-     else{
-         computerSelection = "scissors";
-     }
+    /*    if(computerSelection === 1){
+            computerSelection = "rock";
+        }
+        else if(computerSelection === 2){
+            computerSelection = "paper";
+        }
+        else{
+            computerSelection = "scissors";
+        }
+    */    
+        // console.log(computerSelection);
      return computerSelection;     
+
+     
  }
 
 //This gets the selection of both user and computer and then decides the game
 function getWinner(playerSelection,computerSelection)
 {
-    console.log('Player Selection :', playerSelection);
-    console.log('Computer Selection : ', computerSelection);
+   // console.log('Player Selection :', playerSelection);
+   // console.log('Computer Selection : ', computerSelection);
 
     if(playerSelection === computerSelection)
     {
-        console.log("Its a draw");
+      //  console.log("Its a draw");
     }
     else if(playerSelection === "rock"){
         if(computerSelection === "paper")
         {
-            console.log("You Lose! Paper beats Rock");
+           // console.log("You Lose! Paper beats Rock");
             computerScore++;
 
         }
         else
         {
-            console.log("You Win! Rock beats Scissors");
+           // console.log("You Win! Rock beats Scissors");
             playerScore++;
         }
     }
     else if(playerSelection === "scissors" || playerSelection === "scissor" ){
         if(computerSelection === "paper")
         {
-            console.log("You Win! Scissors beats Paper");
+            //console.log("You Win! Scissors beats Paper");
             playerScore++;
 
         }
         else
         {
-            console.log("You Lose! Rock beats Scissors");
+            //console.log("You Lose! Rock beats Scissors");
             computerScore++;
         }
     }
-    console.log('Player Score :', playerScore);
-    console.log('Computer Score : ', computerScore);   
+   // console.log('Player Score :', playerScore);
+   // console.log('Computer Score : ', computerScore);   
 }
 
 //This function is called to play a round of game
-function playRound(){
-    playerSelection = getPlayerSelection();
-    computerSelection = getComputerChoice();
-    getWinner(playerSelection,computerSelection);
+
+
+
+function playRound(playerSelected,compChoice){
+    let playerSelection = playerSelected;
+    console.log('Player Selection', playerSelection);
+    //getComputerChoice();
+    console.log('Computer Selection',compChoice);
+    getWinner(playerSelection,compChoice);
 }
 
-//To loop the game 5 times
-function game(){
-    for(let i=0;i<5;i++)
-    {
-        playRound();
-    }
-    if(playerScore> 2)
-    {
-        console.log("You win the game");
-    }
-    else if(computerScore> 2)
-    {
-        console.log("Computer wins the game");
-    }
-    else{
-        console.log("No one won the game");
-    }
+function getPlayerSelection()
+{    
+    let playerSelected = this.id;
+   // console.log(playerSelected);
+    let compChoice = getComputerChoice();
+    playRound(playerSelected,compChoice);
 }
-
-
+//playRound();
 let computerScore = 0;
 let playerScore = 0;
-game();
