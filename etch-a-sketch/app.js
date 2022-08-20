efa8493ac087f85c02beac093d28fcb296fd938e
Revanth <revanthhs@protonmail.com>
2022-08-21 02:28:02 +0530

    Added grid sizeable funcationality
    Added needed basic funcationality to the project.
    Even if the user tries to enter more than 100 or less than 1, they will
    be re-prompted to enter the correct value

--- etch-a-sketch/app.js
+++ etch-a-sketch/app.js
@@ -1,13 +1,22 @@
 const grids = document.querySelector(".grids");
+const gridSize = document.querySelector("#askUser");
+let gridValue = gridSize.addEventListener("click", promtUser);
 
-function makeRows(rows, cols){
-    grids.style.setProperty('--grid-rows', rows);
-    grids.style.setProperty('--grid-cols', cols);
-
-    for(c=0; c< (rows * cols); c++){
-        let cell = document.createElement("div");
-        grids.appendChild(cell).className = "grid-item";
-    };
-};
-
-makeRows(16,16);
+function makeRows(rows, cols) {
+  grids.style.setProperty("--grid-rows", rows);
+  grids.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
+    grids.classList.remove("grid-item");
+    cell = document.createElement("div");
+    grids.appendChild(cell).className = "grid-item";
+  }
+}
+function promtUser() {
+  Uservalue = prompt("What is the size you want the grid to be? (Max 100)");
+  grids.innerHTML = "";
+  if(Uservalue > 100 || Uservalue < 1){
+    promtUser();
+    grids.innerHTML = "";
+  }
+  makeRows(Uservalue, Uservalue);
+}
