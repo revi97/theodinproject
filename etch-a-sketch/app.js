7f2ac815ca38d817e481feabc2dd76a325074341
Revanth <revanthhs@protonmail.com>
2022-08-21 14:35:03 +0530

    Added the sizeable grid pixel
    The width of the main area will not change but if the user inputs a
    different values, the grid size will change based on the user inputs.

--- etch-a-sketch/app.js
+++ etch-a-sketch/app.js
@@ -1,6 +1,9 @@
 const grids = document.querySelector(".grids");
 const gridSize = document.querySelector("#askUser");
 let gridValue = gridSize.addEventListener("click", promtUser);
+const gridPixel = document.querySelector('.grid-item');
+let root = document.querySelector(':root');
+
 
 function makeRows(rows, cols) {
   grids.style.setProperty("--grid-rows", rows);
@@ -10,7 +13,9 @@ function makeRows(rows, cols) {
     cell = document.createElement("div");
     grids.appendChild(cell).className = "grid-item";
   }
+  setWidth(rows);
 }
+
 function promtUser() {
   Uservalue = prompt("What is the size you want the grid to be? (Max 100)");
   grids.innerHTML = "";
@@ -19,4 +24,20 @@ function promtUser() {
     grids.innerHTML = "";
   }
   makeRows(Uservalue, Uservalue);
+ 
+}
+
+function setWidth(Uservalue)
+{
+  widthValue = 600/Uservalue;
+  console.log('Grid :', widthValue);
+  let nodes = document.querySelectorAll('.grid-item')
+  console.log(document.querySelectorAll('.grid-item').length);
+  for(i= 0 ; i< document.querySelectorAll('.grid-item').length; i++)
+  {
+    nodes[i].style.width = ('--grid-width', widthValue + 'px');
+    nodes[i].style.height = ('--grid-width', widthValue + 'px');
+    console.log(i);
+  }
+
 }
